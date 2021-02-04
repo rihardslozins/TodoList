f19e8b98d5597864369286c8c146455d33c1d632
rihardslozins <rihards.lozins@gmail.com>
2021-02-04 19:53:28 +0200

    ToDoList app working commit

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
